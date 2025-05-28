144e04047713d94479e713f61d8eee3844a5a9af
atanasovByte <S2309011325@sd.uni-vt.bg>
2025-05-28 17:37:14 +0300

    Fixes #3 – added a do/while loop to prevent empty input

--- demo_app/demo_app/Program.cs
+++ demo_app/demo_app/Program.cs
@@ -19,7 +19,7 @@
             } while (string.IsNullOrWhiteSpace(name));
 
             // At this point, name is guaranteed to be non-null and non-whitespace
-            Console.WriteLine($"Hello, {name.ToUpper()} !!");
+            Console.WriteLine($"Hello, {name.ToUpper()} !!!");
             //Solution of issue #3 - added a do while
             //Fixes #3 - added a do while loop to prevent empty input
         }
